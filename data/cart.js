7d98830fb5ee5d73e5a831b46f5d27af3dc3fb1c
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-04-29 20:19:57 +0530

    Refactor cart management and add tests for addToCart functionality

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,24 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [
-        {
-            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c10',
-            quantity: 2,
-            deliveryOptionsId: '1'
-        },
-        {
-            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity: 1,
-            deliveryOptionsId: '2'
-        }
-    ]
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c10',
+                quantity: 1,
+                deliveryOptionsId: '1'
+            },
+            {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 2,
+                deliveryOptionsId: '2'
+            }
+        ]
+    }
 }
 
 function saveToStorage() {
@@ -20,20 +26,22 @@ function saveToStorage() {
 }
 
 export function addToCart(productId, selectedQuantity) {
-    let flag = 0;
+    let matchingItem;
 
     cart.forEach((cartItem) => {
-        if (cartItem.productId === productId) {
-            cartItem.quantity += Number(selectedQuantity.value);
-            flag = 1;
-        }
+        if(cartItem.productId === productId) matchingItem = cartItem;
     });
 
-    if (flag === 0) {
+    const quantityToAdd = Number(
+        typeof selectedQuantity === 'object' ? selectedQuantity.value : selectedQuantity
+    );
+
+    if(matchingItem) matchingItem.quantity += quantityToAdd;
+    else {
         cart.push({
             productId,
-            quantity: Number(selectedQuantity.value),
-            deliveryOptionsId: '1'
+            quantity : quantityToAdd,
+            deliveryOptionsId : '1'
         });
     }
 
