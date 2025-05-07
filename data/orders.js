20bcb80f76e38244637251ba8f8ac395f9ec7d7b
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-05-07 23:37:08 +0530

    Implement order placement functionality: add event listener for order button to handle order submission and storage

---
+++ data/orders.js
@@ -0,0 +1,9 @@
+export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order) {
+    orders.unshift(order);
+}
+
+export function saveToStorage() {
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder, saveToStorage } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
     let productPriceCents = 0;
@@ -71,11 +72,36 @@ export function renderPaymentSummary() {
             </div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary
+            js-place-order">
             Place your order
         </button>
     `;
 
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML; 
+
+    document.querySelector('.js-place-order')
+        .addEventListener('click', async() => {
+            try {
+                const response = await fetch('https://supersimplebackend.dev/orders', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        cart: cart
+                    })
+                });
+
+                const order = await response.json();
+                addOrder(order);
+                saveToStorage();
+
+            } catch (error) {
+                console.log('Unexpected error occured. Try again later.');
+            }
+            
+            window.location.href = 'orders.html';
+        });
 }
